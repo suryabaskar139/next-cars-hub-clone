5806ebe14e1524845761a6313e6517ae4bca5d5d
Surya Baskar <suryabaskar139@gmail.com>
2024-09-22 01:36:12 +0530

    Initial Push

---
+++ components/CustomButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({title,containerStyles,handleClick}: CustomButtonProps) => {
+  return (
+    <button
+     disabled={false}
+     type={"button"}
+     className={`custom-btn ${containerStyles}`}
+     onClick={handleClick}
+    >
+        <span>{title}</span>
+    </button>
+  )
+}
+
+export default CustomButton
